4bf05221629f6ea639e05446f6a28e38ba5b5963
uventura <ualivent@gmail.com>
2025-04-29 21:50:59 -0300

    Adding basic structure

---
+++ lexer/tokens.h
@@ -0,0 +1,41 @@
+#ifndef CMINUS_TOKENS
+#define CMINUS_TOKENS
+
+//|===============|
+//|    Keywords   |
+//|===============|
+#define IF 0
+#define ELSE 1
+#define INT 2
+#define RETURN 3
+#define VOID 4
+#define WHILE 5
+
+//|===============|
+//|    SYMBOLS    |
+//|===============|
+#define PLUS 6
+#define MINUS 7
+#define MULTIPLY 8
+#define DIVIDE 9
+#define LESS 10
+#define LESS_EQUAL 11
+#define GREATER 12
+#define GREATER_EQUAL 13
+#define EQUAL 14
+#define SEMICOLON 15
+#define COMMA 16
+#define OPEN_PARENTHESIS 17
+#define CLOSE_PARENTHESIS 18
+#define OPEN_BRACKET 19
+#define CLOSE_BRACKET 20
+#define OPEN_BRACES 21
+#define CLOSE_BRACES 22
+
+//|===============|
+//|    COMMENT    |
+//|===============|
+#define START_COMMENT 23
+#define CLOSE_COMMENT 24
+
+#endif
---
+++ utils/debug.h
@@ -0,0 +1,7 @@
+#ifndef CMINUS_DEBUG
+#define CMINUS_DEBUG
+
+void debug_print(char** message);
+void token_debug_print(char** token);
+
+#endif
