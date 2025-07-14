15039eec02bcc41715c737a599e55374e760ae62
GKenjiMizuno <gkenji.mizuno@gmail.com>
2025-07-14 13:09:00 -0300

    Testes casos

--- cminus.tm
+++ cminus.tm
@@ -2,16 +2,36 @@
   1:  ST  0,0(0) 	* zero mem[0]
   2:  LDC  0,10(0) 	* const
   3:  ST  0,0(5) 	* store
-  4:  LD  0,0(5) 	* load
-  5:  LDA  1,0(0) 	* push temp to register
-  6:  LD  0,0(5) 	* load
-  7:  LDA  1,0(1) 	* pop temp from register to AC1
-  8:  ADD  0,1,0 	* +
-  9:  ST  0,1(5) 	* store
- 10:  LD  0,1(5) 	* load
- 11:  LDA  1,0(0) 	* push temp to register
- 12:  LD  0,0(5) 	* load
- 13:  LDA  1,0(1) 	* pop temp from register to AC1
- 14:  ADD  0,1,0 	* +
- 15:  ST  0,0(5) 	* store
- 16:  HALT  0,0,0 	* finish
+  4:  LDC  0,2(0) 	* const
+  5:  ST  0,1(5) 	* store
+  6:  LDC  0,3(0) 	* const
+  7:  ST  0,2(5) 	* store
+  8:  LDC  0,4(0) 	* const
+  9:  ST  0,3(5) 	* store
+ 10:  LDC  0,5(0) 	* const
+ 11:  ST  0,4(5) 	* store
+ 12:  LDC  0,6(0) 	* const
+ 13:  ST  0,5(5) 	* store
+ 14:  LD  0,0(5) 	* load
+ 15:  LDA  1,0(0) 	* push temp to register
+ 16:  LD  0,1(5) 	* load
+ 17:  LDA  1,0(1) 	* pop temp from register to AC1
+ 18:  ADD  0,1,0 	* +
+ 19:  LDA  1,0(0) 	* push temp to register
+ 20:  LD  0,2(5) 	* load
+ 21:  LDA  1,0(1) 	* pop temp from register to AC1
+ 22:  ADD  0,1,0 	* +
+ 23:  LDA  1,0(0) 	* push temp to register
+ 24:  LD  0,3(5) 	* load
+ 25:  LDA  1,0(1) 	* pop temp from register to AC1
+ 26:  ADD  0,1,0 	* +
+ 27:  LDA  1,0(0) 	* push temp to register
+ 28:  LD  0,4(5) 	* load
+ 29:  LDA  1,0(1) 	* pop temp from register to AC1
+ 30:  ADD  0,1,0 	* +
+ 31:  LDA  1,0(0) 	* push temp to register
+ 32:  LD  0,5(5) 	* load
+ 33:  LDA  1,0(1) 	* pop temp from register to AC1
+ 34:  ADD  0,1,0 	* +
+ 35:  ST  0,6(5) 	* store
+ 36:  HALT  0,0,0 	* finish
--- output.asm
+++ output.asm
@@ -1,58 +1,129 @@
 LEXER: Found ID 'a'
-PARSER: Creating ID node from 'a' (pointer: 0x5ff057e3b790)
-PARSER: Set node name to 0x5ff057e3b800
+PARSER: Creating ID node from 'a' (pointer: 0x63ccd4f5c790)
+PARSER: Set node name to 0x63ccd4f5c800
 DECLARATION: Set type 0 for identifier a
 LEXER: Found ID 'b'
-PARSER: Creating ID node from 'b' (pointer: 0x5ff057e3b8c0)
-PARSER: Set node name to 0x5ff057e3b930
+PARSER: Creating ID node from 'b' (pointer: 0x63ccd4f5c8c0)
+PARSER: Set node name to 0x63ccd4f5c930
 DECLARATION: Set type 0 for identifier b
+LEXER: Found ID 'c'
+PARSER: Creating ID node from 'c' (pointer: 0x63ccd4f5c9f0)
+PARSER: Set node name to 0x63ccd4f5ca60
+DECLARATION: Set type 0 for identifier c
+LEXER: Found ID 'd'
+PARSER: Creating ID node from 'd' (pointer: 0x63ccd4f5cb20)
+PARSER: Set node name to 0x63ccd4f5cb90
+DECLARATION: Set type 0 for identifier d
+LEXER: Found ID 'e'
+PARSER: Creating ID node from 'e' (pointer: 0x63ccd4f5cc50)
+PARSER: Set node name to 0x63ccd4f5ccc0
+DECLARATION: Set type 0 for identifier e
+LEXER: Found ID 'f'
+PARSER: Creating ID node from 'f' (pointer: 0x63ccd4f5cd80)
+PARSER: Set node name to 0x63ccd4f5cdf0
+DECLARATION: Set type 0 for identifier f
+LEXER: Found ID 'result'
+PARSER: Creating ID node from 'result' (pointer: 0x63ccd4f5ceb0)
+PARSER: Set node name to 0x63ccd4f5cf20
+DECLARATION: Set type 0 for identifier result
 LEXER: Found ID 'a'
-PARSER: Creating ID node from 'a' (pointer: 0x5ff057e3b9a0)
-PARSER: Set node name to 0x5ff057e3ba10
+PARSER: Creating ID node from 'a' (pointer: 0x63ccd4f5cf90)
+PARSER: Set node name to 0x63ccd4f5d000
 LEXER: Found ID 'b'
-PARSER: Creating ID node from 'b' (pointer: 0x5ff057e3bad0)
-PARSER: Set node name to 0x5ff057e3bb40
+PARSER: Creating ID node from 'b' (pointer: 0x63ccd4f5d0c0)
+PARSER: Set node name to 0x63ccd4f5d130
+LEXER: Found ID 'c'
+PARSER: Creating ID node from 'c' (pointer: 0x63ccd4f5d1f0)
+PARSER: Set node name to 0x63ccd4f5d260
+LEXER: Found ID 'd'
+PARSER: Creating ID node from 'd' (pointer: 0x63ccd4f5d320)
+PARSER: Set node name to 0x63ccd4f5d390
+LEXER: Found ID 'e'
+PARSER: Creating ID node from 'e' (pointer: 0x63ccd4f5d450)
+PARSER: Set node name to 0x63ccd4f5d4c0
+LEXER: Found ID 'f'
+PARSER: Creating ID node from 'f' (pointer: 0x63ccd4f5d580)
+PARSER: Set node name to 0x63ccd4f5d5f0
+LEXER: Found ID 'result'
+PARSER: Creating ID node from 'result' (pointer: 0x63ccd4f5d6b0)
+PARSER: Set node name to 0x63ccd4f5d720
 LEXER: Found ID 'a'
-PARSER: Creating ID node from 'a' (pointer: 0x5ff057e3bb60)
-PARSER: Set node name to 0x5ff057e3bbd0
-LEXER: Found ID 'a'
-PARSER: Creating ID node from 'a' (pointer: 0x5ff057e3bbf0)
-PARSER: Set node name to 0x5ff057e3bc60
-LEXER: Found ID 'a'
-PARSER: Creating ID node from 'a' (pointer: 0x5ff057e3bd20)
-PARSER: Set node name to 0x5ff057e3bd90
+PARSER: Creating ID node from 'a' (pointer: 0x63ccd4f5d740)
+PARSER: Set node name to 0x63ccd4f5d7b0
 LEXER: Found ID 'b'
-PARSER: Creating ID node from 'b' (pointer: 0x5ff057e3bdb0)
-PARSER: Set node name to 0x5ff057e3be20
-LEXER: Found ID 'a'
-PARSER: Creating ID node from 'a' (pointer: 0x5ff057e3be40)
-PARSER: Set node name to 0x5ff057e3beb0
+PARSER: Creating ID node from 'b' (pointer: 0x63ccd4f5d7d0)
+PARSER: Set node name to 0x63ccd4f5d840
+LEXER: Found ID 'c'
+PARSER: Creating ID node from 'c' (pointer: 0x63ccd4f5d8b0)
+PARSER: Set node name to 0x63ccd4f5d920
+LEXER: Found ID 'd'
+PARSER: Creating ID node from 'd' (pointer: 0x63ccd4f5d990)
+PARSER: Set node name to 0x63ccd4f5da00
+LEXER: Found ID 'e'
+PARSER: Creating ID node from 'e' (pointer: 0x63ccd4f5da70)
+PARSER: Set node name to 0x63ccd4f5dae0
+LEXER: Found ID 'f'
+PARSER: Creating ID node from 'f' (pointer: 0x63ccd4f5db50)
+PARSER: Set node name to 0x63ccd4f5dbc0
 Program node creation
-Root node set to 0x5ff057e3c010
+Root node set to 0x63ccd4f5dd20
 Program syntax is correct!
 
 ===== Syntactic Tree =====
-Program  (line 8)
-  Block  (line 8)
-    Declaration  (line 2)
-      Type  [dataType=int]  (line 2)
-      Identifier  [name="a"]  (line 2)
+Program  (line 21)
+  Block  (line 21)
     Declaration  (line 3)
       Type  [dataType=int]  (line 3)
-      Identifier  [name="b"]  (line 3)
-    Assign  (line 5)
-      Identifier  [name="a"]  (line 5)
-      Number  [value=10]  (line 5)
-    Assign  (line 6)
-      Identifier  [name="b"]  (line 6)
-      Plus  (line 6)
-        Identifier  [name="a"]  (line 6)
-        Identifier  [name="a"]  (line 6)
-    Assign  (line 7)
-      Identifier  [name="a"]  (line 7)
-      Plus  (line 7)
-        Identifier  [name="b"]  (line 7)
-        Identifier  [name="a"]  (line 7)
+      Identifier  [name="a"]  (line 3)
+    Declaration  (line 4)
+      Type  [dataType=int]  (line 4)
+      Identifier  [name="b"]  (line 4)
+    Declaration  (line 5)
+      Type  [dataType=int]  (line 5)
+      Identifier  [name="c"]  (line 5)
+    Declaration  (line 6)
+      Type  [dataType=int]  (line 6)
+      Identifier  [name="d"]  (line 6)
+    Declaration  (line 7)
+      Type  [dataType=int]  (line 7)
+      Identifier  [name="e"]  (line 7)
+    Declaration  (line 8)
+      Type  [dataType=int]  (line 8)
+      Identifier  [name="f"]  (line 8)
+    Declaration  (line 9)
+      Type  [dataType=int]  (line 9)
+      Identifier  [name="result"]  (line 9)
+    Assign  (line 11)
+      Identifier  [name="a"]  (line 11)
+      Number  [value=10]  (line 11)
+    Assign  (line 12)
+      Identifier  [name="b"]  (line 12)
+      Number  [value=2]  (line 12)
+    Assign  (line 13)
+      Identifier  [name="c"]  (line 13)
+      Number  [value=3]  (line 13)
+    Assign  (line 14)
+      Identifier  [name="d"]  (line 14)
+      Number  [value=4]  (line 14)
+    Assign  (line 15)
+      Identifier  [name="e"]  (line 15)
+      Number  [value=5]  (line 15)
+    Assign  (line 16)
+      Identifier  [name="f"]  (line 16)
+      Number  [value=6]  (line 16)
+    Assign  (line 18)
+      Identifier  [name="result"]  (line 18)
+      Plus  (line 18)
+        Plus  (line 18)
+          Plus  (line 18)
+            Plus  (line 18)
+              Plus  (line 18)
+                Identifier  [name="a"]  (line 18)
+                Identifier  [name="b"]  (line 18)
+              Identifier  [name="c"]  (line 18)
+            Identifier  [name="d"]  (line 18)
+          Identifier  [name="e"]  (line 18)
+        Identifier  [name="f"]  (line 18)
 ============================
 
 Semantic analysis:
@@ -65,11 +136,39 @@ DEBUG: Added to symbol table - a: 0
 	DEBUG: Checking declaration, name: b
 DEBUG: Setting type 0 for identifier b
 DEBUG: Added to symbol table - b: 0
+	DEBUG: case NDeclaration
+	DEBUG: Checking declaration, name: c
+DEBUG: Setting type 0 for identifier c
+DEBUG: Added to symbol table - c: 0
+	DEBUG: case NDeclaration
+	DEBUG: Checking declaration, name: d
+DEBUG: Setting type 0 for identifier d
+DEBUG: Added to symbol table - d: 0
+	DEBUG: case NDeclaration
+	DEBUG: Checking declaration, name: e
+DEBUG: Setting type 0 for identifier e
+DEBUG: Added to symbol table - e: 0
+	DEBUG: case NDeclaration
+	DEBUG: Checking declaration, name: f
+DEBUG: Setting type 0 for identifier f
+DEBUG: Added to symbol table - f: 0
+	DEBUG: case NDeclaration
+	DEBUG: Checking declaration, name: result
+DEBUG: Setting type 0 for identifier result
+DEBUG: Added to symbol table - result: 0
 	DEBUG: case NAssignStmt / NAssign
 DEBUG: Assignment type check - lhs: a (int), rhs: int
 	DEBUG: case NAssignStmt / NAssign
 DEBUG: Assignment type check - lhs: b (int), rhs: int
 	DEBUG: case NAssignStmt / NAssign
-DEBUG: Assignment type check - lhs: a (int), rhs: int
+DEBUG: Assignment type check - lhs: c (int), rhs: int
+	DEBUG: case NAssignStmt / NAssign
+DEBUG: Assignment type check - lhs: d (int), rhs: int
+	DEBUG: case NAssignStmt / NAssign
+DEBUG: Assignment type check - lhs: e (int), rhs: int
+	DEBUG: case NAssignStmt / NAssign
+DEBUG: Assignment type check - lhs: f (int), rhs: int
+	DEBUG: case NAssignStmt / NAssign
+DEBUG: Assignment type check - lhs: result (int), rhs: int
 Program is semantically correct!
 Generating code:
